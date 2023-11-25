e791c3b3bdf135b50c8434e309c4fcb34efc7f0b
Farhan Rifaz <farhanrifaz@Farhans-MacBook-Pro.local>
2023-11-25 19:46:30 +0600

    added jwt

--- index.js
+++ index.js
@@ -7,49 +7,90 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 3000;
 
+
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// custom middlewares
+const verifyToken = (req, res, next) => {
+    // console.log('inside verify token', req.headers.authorization);
+    if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+const verifyAdmin = async (req, res, next) => {
+    const email = req.decoded.email;
+    const query = { email: email };
+    const user = await userCollection.findOne(query);
+    const isAdmin = user?.role === 'admin';
+    if (!isAdmin) {
+        return res.status(403).send({ message: 'forbidden access' });
+    }
+    next();
+}
 
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0zrhnww.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
-const packageCollection = client.db('embark-escapes-db').collection('tourPackages')
-const guideCollection = client.db('embark-escapes-db').collection('guides')
-
 async function run() {
-  try {
 
-    app.get('/packages', async(req, res) => {
-        const result = await packageCollection.find().toArray();
-        res.send(result)
-    })
-    app.get('/guides', async(req, res) => {
-        const result = await guideCollection.find().toArray();
-        res.send(result)
-    })
+    const packageCollection = client.db('embark-escapes-db').collection('tourPackages')
+    const guideCollection = client.db('embark-escapes-db').collection('guides')
+    const userCollection = client.db('embark-escapes-db').collection('users')
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
+    try {
 
-  }
+        app.get('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+            res.send({ token })
+        })
+
+        app.get('/packages', async (req, res) => {
+            const result = await packageCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/packages', async (req, res) => {
+
+        })
+
+        app.get('/guides', async (req, res) => {
+            const result = await guideCollection.find().toArray();
+            res.send(result)
+        })
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
 }
 run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
     res.send('assignment-12 server is running')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`assignment-12 is running on port ${port}`);
-  })
+})
